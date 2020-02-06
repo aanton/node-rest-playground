d92b419334ab912e7a415dc5acd5aecc145114d4
aanton <aanton@gmail.com>
2020-02-06 22:29:12 +0100

    Refactor routes in its own file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import sequelize, { Post } from './models';
+
+import sequelize from './models';
+import postsRoutes from './routes/posts';
 
 dotenv.config();
 const app = express();
@@ -12,53 +14,7 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
 // Routes
-app.get('/api/posts', async (req, res) => {
-  const posts = await Post.findAll();
-  res.json(posts);
-});
-
-app.get('/api/post/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = await Post.findByPk(id);
-  if (!post) {
-    return res.status(404).send({ error: `Post ${id} not found` });
-  }
-
-  res.json(post);
-});
-
-app.post('/api/posts', async (req, res) => {
-  const post = await Post.create({
-    title: req.body.title,
-  });
-
-  res.json(post);
-});
-
-app.put('/api/post/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = await Post.findByPk(id);
-  if (!post) {
-    return res.status(404).send({ error: `Post ${id} not found` });
-  }
-
-  post.title = req.body.title;
-  await post.save();
-
-  res.json(post);
-});
-
-app.delete('/api/post/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = await Post.findByPk(id);
-  if (!post) {
-    return res.status(404).send({ error: `Post ${id} not found` });
-  }
-
-  await post.destroy();
-
-  res.json(post);
-});
+app.use('/api', postsRoutes);
 
 // Start server
 
---
+++ src/routes/posts.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import { Post } from '../models';
+
+const router = express.Router();
+
+router.get('/posts', async (req, res) => {
+  const posts = await Post.findAll();
+  res.json(posts);
+});
+
+router.get('/post/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = await Post.findByPk(id);
+  if (!post) {
+    return res.status(404).send({ error: `Post ${id} not found` });
+  }
+
+  res.json(post);
+});
+
+router.post('/posts', async (req, res) => {
+  const post = await Post.create({
+    title: req.body.title,
+  });
+
+  res.json(post);
+});
+
+router.put('/post/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = await Post.findByPk(id);
+  if (!post) {
+    return res.status(404).send({ error: `Post ${id} not found` });
+  }
+
+  post.title = req.body.title;
+  await post.save();
+
+  res.json(post);
+});
+
+router.delete('/post/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = await Post.findByPk(id);
+  if (!post) {
+    return res.status(404).send({ error: `Post ${id} not found` });
+  }
+
+  await post.destroy();
+
+  res.json(post);
+});
+
+export default router;
