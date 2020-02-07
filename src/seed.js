2018dafdec9876159c8920c7903ff3f85fd4ab09
aanton <aanton@20minutos.es>
2020-02-07 09:14:37 +0100

    Refactor seed script to use IIFE

--- src/seed.js
+++ src/seed.js
@@ -1,12 +1,12 @@
 import sequelize, { Post } from './models';
 import faker from 'faker';
 
-const recreateDatabase = async function() {
+const recreate = async () => {
   await Post.sync({ force: true });
   console.log(`Database recreated`);
 };
 
-const seedDatabase = async function() {
+const seed = async () => {
   const posts = Array.from({ length: 5 }).map(() => {
     return {
       title: faker.lorem.sentence(),
@@ -17,14 +17,8 @@ const seedDatabase = async function() {
   console.log(`Database seeded`);
 };
 
-sequelize
-  .authenticate()
-  .then(async () => {
-    console.log(`Database connection has been established successfully`);
-
-    await recreateDatabase();
-    await seedDatabase();
-  })
-  .catch(err => {
-    console.error(`Unable to connect to the database`, err);
-  });
+(async () => {
+  await sequelize.authenticate();
+  await recreate();
+  await seed();
+})();
