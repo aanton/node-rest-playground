de6220ed3fb89a9189a3d52aa7f22c7f4acecd04
aanton <aanton@20minutos.es>
2020-02-11 23:17:48 +0100

    Use import instead of require

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
-const { Sequelize, DataTypes } = require('sequelize');
+import { Sequelize, DataTypes } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DATABASE_URI, {
   logging: false,
