98d1e5e2f92d5ccc83bb9d1ed407244059aadf41
aanton <aanton@20minutos.es>
2020-02-12 08:44:29 +0100

    Group tests in describe blocks

--- tests/posts.test.js
+++ tests/posts.test.js
@@ -8,29 +8,32 @@ beforeAll(async () => {
   await sequelize.sync({ force: true });
 });
 
-it('Should save post to database', async done => {
-  const post = {
-    title: 'My first test post',
-  };
+describe('POST /api/posts', () => {
+  it('Should save post to database', async done => {
+    const post = {
+      title: 'My first test post',
+    };
 
-  const response = await request
-    .post('/api/posts')
-    .set('Content-type', 'application/json')
-    .send(post);
+    const response = await request
+      .post('/api/posts')
+      .set('Content-type', 'application/json')
+      .send(post);
 
-  // Check response
-  expect(response.status).toBe(200);
-  expect(typeof response.body.id).toBe('number');
-  expect(response.body.title).toBe(post.title);
-  expect(response.body.createdAt).toBeTruthy();
-  expect(response.body.updatedAt).toBeTruthy();
+    // Check response
+    expect(response.status).toBe(200);
+    expect(typeof response.body.id).toBe('number');
+    expect(response.body.title).toBe(post.title);
+    expect(response.body.createdAt).toBeTruthy();
+    expect(response.body.updatedAt).toBeTruthy();
 
-  // Search the post in the database
-  const dbPost = await Post.findByPk(response.body.id);
-  expect(response.body.id).toBe(dbPost.id);
-  expect(response.body.title).toBe(dbPost.title);
-  expect(dbPost.createdAt).toBeTruthy();
-  expect(dbPost.updatedAt).toBeTruthy();
+    // Search the post in the database
+    const dbPost = await Post.findByPk(response.body.id);
+    expect(response.body.id).toBe(dbPost.id);
+    expect(response.body.title).toBe(dbPost.title);
+    expect(dbPost.createdAt).toBeTruthy();
+    expect(dbPost.updatedAt).toBeTruthy();
 
-  done();
+    done();
+  });
+
 });
