0836c41401a6c7c18d5fe4763ce4990528e5c7d8
aanton <aanton@gmail.com>
2020-02-12 21:23:47 +0100

    Refactor "create a new post" test

--- tests/posts.test.js
+++ tests/posts.test.js
@@ -4,45 +4,18 @@ import supertest from 'supertest';
 
 const request = supertest(app);
 
-beforeAll(async () => {
+beforeEach(async () => {
   await sequelize.sync({ force: true });
 });
 
-describe('POST /api/posts', () => {
-  it('Should save post to database', async done => {
-    const post = {
-      title: 'My first test post',
-    };
+describe('Creates a new post', () => {
+  it('Fails if the title is missing', async done => {
+    const data = {};
 
     const response = await request
       .post('/api/posts')
       .set('Content-type', 'application/json')
-      .send(post);
-
-    // Check response
-    expect(response.status).toBe(200);
-    expect(typeof response.body.id).toBe('number');
-    expect(response.body.title).toBe(post.title);
-    expect(response.body.createdAt).toBeTruthy();
-    expect(response.body.updatedAt).toBeTruthy();
-
-    // Search the post in the database
-    const dbPost = await Post.findByPk(response.body.id);
-    expect(response.body.id).toBe(dbPost.id);
-    expect(response.body.title).toBe(dbPost.title);
-    expect(dbPost.createdAt).toBeTruthy();
-    expect(dbPost.updatedAt).toBeTruthy();
-
-    done();
-  });
-
-  it('Should fail if title is missing', async done => {
-    const wrongPost = {};
-
-    const response = await request
-      .post('/api/posts')
-      .set('Content-type', 'application/json')
-      .send(wrongPost);
+      .send(data);
 
     expect(response.status).toBe(500);
     expect(response.body.error).toBeTruthy();
@@ -51,19 +24,47 @@ describe('POST /api/posts', () => {
     done();
   });
 
-  it('Should fail if title is empty', async done => {
-    const wrongPost = {
+  it('Fails if the title is empty', async done => {
+    const data = {
       title: '',
     };
 
     const response = await request
       .post('/api/posts')
       .set('Content-type', 'application/json')
-      .send(wrongPost);
+      .send(data);
 
     expect(response.status).toBe(500);
     expect(response.body.error).toBeTruthy();
     expect(response.body.error).toMatch(/SequelizeValidationError/);
+
+    done();
+  });
+
+  it('Saves a post in the database & returns it with all its attributes', async done => {
+    const data = {
+      title: 'My first test post',
+    };
+
+    const response = await request
+      .post('/api/posts')
+      .set('Content-type', 'application/json')
+      .send(data);
+
+    // Check response
+    expect(response.status).toBe(200);
+    expect(typeof response.body.id).toBe('number');
+    expect(response.body.title).toBe(data.title);
+    expect(response.body.createdAt).toBeTruthy();
+    expect(response.body.updatedAt).toBeTruthy();
+    expect(response.body.createdAt).toBe(response.body.updatedAt);
+
+    // Search the post in the database
+    const dbPost = await Post.findByPk(response.body.id);
+    expect(dbPost.title).toBe(data.title);
+    expect(dbPost.id).toBe(response.body.id);
+    expect(dbPost.createdAt).toEqual(new Date(response.body.createdAt));
+    expect(dbPost.updatedAt).toEqual(new Date(response.body.updatedAt));
 
     done();
   });
