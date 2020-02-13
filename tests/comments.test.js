dedbcb2781be5c4d3fe35b1aae52689a1b6f413b
aanton <aanton@gmail.com>
2020-02-13 20:04:59 +0100

    Add "search comments" tests

--- tests/comments.test.js
+++ tests/comments.test.js
@@ -33,3 +33,61 @@ describe('Lists all comments', () => {
     done();
   });
 });
+
+describe('Searches comments', () => {
+  const data = [{ text: 'First comment' }, { text: 'Second comment' }];
+
+  beforeEach(async () => {
+    await Comment.create(data[0]);
+    await Comment.create(data[1]);
+  });
+
+  it('Fails if the query is missing', async done => {
+    const response = await request.get(`/api/comments/search`).send();
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Search parameter is empty/invalid');
+
+    done();
+  });
+
+  it('Fails if the query is empty', async done => {
+    const query = '';
+    const response = await request
+      .get(`/api/comments/search?q=${query}`)
+      .send();
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Search parameter is empty/invalid');
+
+    done();
+  });
+
+  it.skip('Fails if the query is too short', {});
+
+  it('Gets an empty list if there are not matched comments', async done => {
+    const query = 'abracadabra';
+    const response = await request
+      .get(`/api/comments/search?q=${query}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([]);
+
+    done();
+  });
+
+  it('Gets all matched comments ordered by newest', async done => {
+    const query = 'comment';
+    const response = await request
+      .get(`/api/comments/search?q=${query}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0]).toEqual(expect.objectContaining(data[1]));
+    expect(response.body[1]).toEqual(expect.objectContaining(data[0]));
+
+    done();
+  });
+});
